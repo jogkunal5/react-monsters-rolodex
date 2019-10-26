b1f90952ee48d4e65b765d33f18eff9d8b213a15
jogkunal5 <jogkunal5@gmail.com>
2019-10-26 19:53:37 +0530

    Updates

---
+++ precache-manifest.cdbca0ade647f05ef9e5f54ecf0176f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80da76a8a72048f6291d6f2080620ec0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8501685ab66070be260e",
+    "url": "/monsters-rolodex/static/css/main.07982e69.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "8501685ab66070be260e",
+    "url": "/monsters-rolodex/static/js/main.86aa0deb.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
