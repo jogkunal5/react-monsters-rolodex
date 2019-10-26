887d3be3c2deecffdf1af6478fc8a8fd1f18b74f
jogkunal5 <jogkunal5@gmail.com>
2019-10-26 11:05:18 +0530

    New changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -7,20 +8,37 @@ class App extends Component {
     super(); // it calls the constructor method on the Component class and gives access of this.state
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
+  // This is lifecycle method of react and it gets called when component gets initialized.
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value }, () => {
+      console.log(this.state);
+    });
+  }
+
   render() {
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
-      <div className="App" >                        
-        <CardList monsters={this.state.monsters}></CardList> {/* Passing monsters object to CardList component */}
+      <div className="App" >
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder='Search Monsters' handleChange={this.handleChange}>
+        </SearchBox>
+        <CardList monsters={filteredMonsters}></CardList> {/* Passing monsters object to CardList component */}
       </div>
     );
   }
