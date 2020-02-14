e91d5eacbd48565857abc5e289d9de7af41a03aa
jogkunal5 <jogkunal5@gmail.com>
2020-02-14 09:34:01 +0530

    Implemented Redux

--- src/App.js
+++ src/App.js
@@ -1,47 +1,68 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super(); // it calls the constructor method on the Component class and gives access of this.state
+import { setSearchField, requestRobots } from './actions';
 
-    this.state = {
-      monsters: [],
-      searchField: ''
-    }
+// what state to listen to
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
+}
+
+// what action needs to be dispatch based on state
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
+}
+
+class App extends Component {
+  // constructor() {
+  //     super(); // it calls the constructor method on the Component class and gives access of this.state
+
+  //     this.state = {
+  //       monsters: []
+  //     }
+  // }
 
   // This is lifecycle method of react and it gets called when component gets initialized.
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }));
-  }
-
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value }, () => {
-      console.log(this.state);
-    });
+    this.props.onRequestRobots();
   }
 
   render() {
 
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
+    const { robots, searchField, onSearchChange, isPending } = this.props;
+
+    const filteredMonsters = robots.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
 
-    return (
-      <div className="App" >
-        <h1>Monsters Rolodex</h1>
-        <SearchBox placeholder='Search Monsters' handleChange={this.handleChange}>
-        </SearchBox>
-        <CardList monsters={filteredMonsters}></CardList> {/* Passing monsters object to CardList component */}
-      </div>
-    );
+    return isPending ?
+      <h1>Loading</h1> :
+      (
+        <div className="App" >
+          <h1>Monsters Rolodex</h1>
+          <SearchBox placeholder='Search Monsters' handleChange={onSearchChange}>
+          </SearchBox>
+          <CardList monsters={filteredMonsters}></CardList> {/* Passing monsters object to CardList component */}
+        </div>
+      );
   }
 }
 
-export default App;
+/** 
+ * connect() is a higher order function (higher order function: a function that return another function)
+ * @mapStateToProps: state to listen
+ * @mapDispatchToProps: state to dispatch
+ **/
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
